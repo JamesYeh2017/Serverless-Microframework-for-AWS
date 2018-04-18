472deb063d096f6446b22c518877eca60fee427b
JamesYeh <h4587074@yahoo.com.tw>
2018-04-18 18:29:19 +0800

    add annotation in neo4j_db.py

--- neo4j_db.py
+++ neo4j_db.py
@@ -1,5 +1,12 @@
 from neo4j.v1 import GraphDatabase
 
+"""
+Deploy to AWS : https://docs.aws.amazon.com/zh_cn/lambda/latest/dg/lambda-python-how-to-create-deployment-package.html
+在aws上開啟一個ec2並install neo4j，並將此module deploy to lambda，最後將neo4j driver binding to ec2 of neo4j；
+Create topic and Publish topic.
+Use SNS to trigger lambda and Inspect the SNS msg aleardy storage in the neo4j database.
+"""
+
 
 def handler(event, context):
 
